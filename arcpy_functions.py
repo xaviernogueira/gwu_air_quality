5601d24d185c5c111418550f716ca98150075d9a
xaviernogueira <xavier.rojas.nogueira@gmail.com>
2021-11-08 13:51:45 -0500

    Created batch project function

--- arcpy_functions.py
+++ arcpy_functions.py
@@ -2,6 +2,7 @@ import os
 import arcpy
 import logging
 
+
 def init_logger(filename):
     """Initializes logger w/ same name as python file"""
 
@@ -86,9 +87,10 @@ def batch_resample_or_aggregate(in_folder, cell_size, out_folder='', str_in='.ti
                     out_ras = arcpy.sa.Resample(out_agg, 'Average', output_cellsize=cell_size)
 
                 # Save the output
-                out_txt.write('%s -> %s' % (name, out_file))
+                out_txt.write('\n %s -> %s\n' % (name, out_file))
                 out_ras.save(out_file)
                 logging.info('Aggregated %s' % name)
+                out_txt.close()
 
         except arcpy.ExecuteError:
             logging.info(str(arcpy.GetMessages()))
@@ -103,6 +105,60 @@ def batch_resample_or_aggregate(in_folder, cell_size, out_folder='', str_in='.ti
 
     return out_folder
 
+
+def batch_raster_project(in_folder,  spatial_ref, out_folder='', suffix='_p.tif'):
+    """
+    This function batch projects rasters and places them in a new flder
+    :param in_folder: folder containing .tif rasters
+    :param out_folder: folder to save output rasters
+    :param spatial_ref: a spatial reference file or a raster/shapefile with the desired spatial reference
+    :param suffix: suffix to add to output rasters (_p is default i.e., btw.tif -> btw_p.tif)
+    :return: the out_folder
+    """
+    # initialize logger, environment, and delete files list
+    init_logger(__file__)
+    arcpy.env.overwriteOutput = True
+    del_files = []
+
+    # create list of valid input files
+    all_files = os.listdir(in_folder)
+    in_names = [i for i in all_files if i[-4:] == '.tif']
+    in_files = [in_folder + '\\%s' % i for i in in_names]
+    if len(in_files) == 0:
+        return print('ERROR. No valid input .tif files in %s. Please run again.' % in_folder)
+
+    # create output spatial reference object
+    if isinstance(spatial_ref, str):
+        ext = spatial_ref[-4:]
+        if ext == '.tif' or ext == '.shp':
+            out_sr = arcpy.Describe(spatial_ref).spatialReference
+        else:
+            out_sr = spatial_ref
+
+    else:
+        return print('spatial_ref must be a .tif, .shp, or a arcpy spatial reference object')
+
+    # if out_folder does not exist or is not specified, make new folder
+    if out_folder == '':
+        out_folder = in_folder + '\\projected'
+
+    if not os.path.exists(out_folder):
+        os.makedirs(out_folder)
+
+    # project and save rasters
+    for i, file in enumerate(in_files):
+        name = in_names[i]
+        out_ras = out_folder + '\\%s' % name.replace('.tif', suffix)
+
+        try:
+            arcpy.ProjectRaster_management(file, out_ras, out_coor_system=out_sr, resampling_type='BILINEAR')
+            logging.info('Projected %s -> %s' % (name, out_ras))
+
+        except arcpy.ExecuteError:
+            logging.info(str(arcpy.GetMessages()))
+            logging.info('ERROR, skipped %s' % file)
+
+    return out_folder
 
 def netcdf_to_tiff(in_folder, out_folder=''):
 
@@ -122,8 +178,9 @@ def extract_vals_from_tiff(stations_points, monthly_dir):
 ###################################################################
 dem_folder = r'C:\Users\xrnogueira\Documents\Data\3DEP'
 pop_den = r'C:\Users\xrnogueira\Documents\Data\Population_density'
+dem_for_ref = dem_folder + '\\USGS_1_n25w082.tif'
 
-out_folder = r'C:\Users\xrnogueira\Documents\Data\resampled_popden'
-batch_resample_or_aggregate(in_folder=pop_den, cell_size=0.001, out_folder=out_folder, str_in='.tif', agg=True)
-
+resampled_pop = r'C:\Users\xrnogueira\Documents\Data\resampled_popden'
+#batch_resample_or_aggregate(in_folder=pop_den, cell_size=0.001, out_folder=resampled_pop, str_in='.tif', agg=True)
+batch_raster_project(resampled_pop,  spatial_ref=dem_for_ref, out_folder='', suffix='_p.tif')
 
